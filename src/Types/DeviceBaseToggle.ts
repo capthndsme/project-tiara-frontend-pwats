cf9c45af0813aed8c3f16f242a92557c2e4c24ac
capthndsme <nieoy@msn.com>
2023-04-23 11:01:28 +0800

    Fix manifest. Introduce ToggleType ONEOFF & SWITCH Oneoff toggles performs an action that is does not have an on or off state. For example, a food bowl cleaning action. A switch toggle is a toggle that has an on and off state. For example, a light switch.

--- src/Types/DeviceBaseToggle.ts
+++ src/Types/DeviceBaseToggle.ts
@@ -1,7 +1,13 @@
+export enum ToggleType {
+   SWITCH = "SWITCH", // Simple on/off toggle.
+   ONEOFF = "ONEOFF", // One-off toggle. (Toggles back to off after it finishes executing, like cleaning).
+}
+
 export interface DeviceBaseToggle {
    toggleName: string,
+   toggleDescription?: string,
+   toggleType: ToggleType,
    toggleValue: boolean,
-   toggleDescription: boolean,
-   hasLock: boolean | null,
-   lastChanged: number | null // Null means the toggle has never been changed.
+   lastChanged?: number | undefined, // Null means the toggle has never been changed.
+   hasLock?: boolean | undefined, // Null means the toggle has never been changed.
 }
